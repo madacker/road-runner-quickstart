ff0a528c02e568ba0acfeae5886b1f8464b69768
Andrew <andrewgo@hotmail.com>
2023-11-10 14:30:01 -0800

    Add timing of the Road Runner creation.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,5 +1,8 @@
 package com.example.meepmeeptesting;
 
+import static java.lang.System.nanoTime;
+
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -14,16 +17,23 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
+        long startTime = nanoTime();
+        Action action = myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+//                .lineToX(30)
+//                .turn(Math.toRadians(90))
+//                .lineToY(30)
+//                .turn(Math.toRadians(90))
+//                .lineToX(0)
+//                .turn(Math.toRadians(90))
+//                .lineToY(0)
                 .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
+                .build();
+        long duration = nanoTime() - startTime;
+        double millis = duration / (1000.0 * 1000.0);
+
+        System.out.println(String.format("Milliseconds: %.2f", millis));
+
+        myBot.runAction(action);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
