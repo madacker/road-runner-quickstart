4a4566c961105b320c69cb9e93786aa1ad97a2c3
Andrew <andrewgo@hotmail.com>
2024-03-01 18:09:00 -0800

    Many updates, close to final. The offset is negative for both components though.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opticaltracking/OpticalTrackingTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opticaltracking/OpticalTrackingTuner.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.opticaltracking;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -17,7 +20,7 @@ public class OpticalTrackingTuner extends LinearOpMode {
     final int REVOLUTION_COUNT = 1;
     final double RAMP_TIME = 2; // Seconds
     final double SPIN_SPEED = 0.3;
-    final double PUSH_INCHES = 24; // @@@ 96; // 4 tiles
+    final double PUSH_INCHES = 96; // 4 tiles
 
     boolean aPressed = false;
     boolean bPressed = false;
@@ -54,23 +57,23 @@ public class OpticalTrackingTuner extends LinearOpMode {
     }
 
     Calibration measureCalibration(OpticalTrackingPaa5100 optical, MecanumDrive drive) {
-        Calibration result = new Calibration();
         while (true) {
+            drive.leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            drive.leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            drive.rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            drive.rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
             telemetry.addLine(String.format("Ready for calibration. Push the robot forward in a straight line for %.1f inches.\n", PUSH_INCHES));
             telemetry.addLine("Press A to start");
             telemetry.update();
             while (opModeIsActive() && !buttonA())
                 ;
 
-            drive.leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            drive.leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            drive.rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-            drive.rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
             long xTotal = 0;
             long yTotal = 0;
-
+            Calibration result = new Calibration();
             optical.getMotion(); // Reset the sensor
+
             while (opModeIsActive() && !buttonA()) {
                 // Query the push results:
                 OpticalTrackingPaa5100.Motion motion = optical.getMotion();
@@ -80,14 +83,16 @@ public class OpticalTrackingTuner extends LinearOpMode {
                 double tickDistance = Math.hypot(xTotal, yTotal);
 
                 result.inchesPerTick = PUSH_INCHES / tickDistance;
-                result.correctionAngle = Math.atan2(yTotal, xTotal); // Rise over run
+                result.correctionAngle = -Math.atan2(yTotal, xTotal); // Rise over run
 
-                telemetry.addLine(String.format("Accumulated ticks: (%d, %d), angle: %.2f°", xTotal, yTotal, Math.toDegrees(result.correctionAngle)));
-                telemetry.addLine(String.format("Press A once you've pushed %.1f inches.", PUSH_INCHES));
+                telemetry.addLine(String.format("Accumulated ticks: (%d, %d), angle: %.2f°\n", xTotal, yTotal, Math.toDegrees(result.correctionAngle)));
+                telemetry.addLine(String.format("Press A once you've pushed exactly %.1f inches.", PUSH_INCHES));
                 telemetry.update();
             }
-            telemetry.addLine(String.format("Test result: Inches per tick: %f, correction: %.3f°\n", result.inchesPerTick, Math.toDegrees(result.correctionAngle)));
+
+            telemetry.addLine(String.format("Test result: Inches per tick: %f,\n correction: %.3f°\n", result.inchesPerTick, Math.toDegrees(result.correctionAngle)));
             telemetry.addLine("Press A to continue, B to repeat this test");
+            telemetry.update();
             while (opModeIsActive() && !buttonB()) {
                 if (buttonA())
                     return result; // ====>
@@ -95,126 +100,100 @@ public class OpticalTrackingTuner extends LinearOpMode {
         }
     }
 
-    double measureRadialDistance(OpticalTrackingPaa5100 optical, MecanumDrive drive, Calibration calibration) {
-        telemetry.addLine("Now ready for the spin-distance test.");
-        telemetry.addLine("The robot will need room to spin.\n");
-        telemetry.addLine("Press A to start, B to skip");
-        telemetry.update();
-        while (opModeIsActive() && !buttonA()) {
-            if (buttonB())
-                return -1;
-        }
-
-        telemetry.addLine(String.format("Rotating %d times...", REVOLUTION_COUNT));
-        telemetry.update();
-
-        // Ramp up the spin turning counter-clockwise:
-        rampMotors(drive, true);
-
-        optical.getMotion(); // Zero the motion
-        double tickDistance = 0;
-        double rotationTotal = 0;
-        double rotationTarget = REVOLUTION_COUNT * 2 * Math.PI;
-
-        // Reset the two sensors:
-        drive.imu.resetYaw();
-        optical.getMotion();
-        double lastYaw = drive.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        while (opModeIsActive() && (rotationTotal < rotationTarget)) {
-            // Track the amount of rotation we've done:
-            double yaw = drive.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-            rotationTotal += Globals.normalizeAngle(yaw - lastYaw);
-            lastYaw = yaw;
-
-            // Track the distance we've gone:
-            OpticalTrackingPaa5100.Motion motion = optical.getMotion();
-            tickDistance += Math.hypot(motion.x, motion.y);
-
-            double rotationsRemaining = (rotationTarget - rotationTotal) / (2 * Math.PI);
-            telemetry.addLine(String.format("%.2f rotations remaining, %.0f ticks traveled", rotationsRemaining, tickDistance));
-            telemetry.update();
-        }
-
-        rampMotors(drive, false);
-
-        // Circumference = 2 * radius * pi
-        double radius = tickDistance * calibration.inchesPerTick / (2 * REVOLUTION_COUNT * Math.PI);
-        telemetry.addLine(String.format("Test result: radial distance: %.2f inches\n", radius));
-        return radius;
-    }
-
     static class CenterOfRotation {
         double x;
         double y;
-        double radius;
+        double farthestPointRadius;
+        double traveledRadius;
     }
 
     CenterOfRotation measureCenterOfRotation(OpticalTrackingPaa5100 optical, MecanumDrive drive, Calibration calibration) {
-        telemetry.addLine("Now ready for the center-of-rotation test.");
-        telemetry.addLine("The robot will need room to spin.\n");
-        telemetry.addLine("Press A to start, B to skip");
-        telemetry.update();
-        while (opModeIsActive() && !buttonA()) {
-            if (buttonB())
-                return null;
-        }
-
-        telemetry.addLine(String.format("Rotating %d times...", REVOLUTION_COUNT));
-        telemetry.update();
-
-        ArrayList<Point> points = new ArrayList<>();
-        rampMotors(drive, true);
-
-        Point currentPoint = new Point(0, 0);
-        double rotationTotal = 0;
-        double rotationTarget = REVOLUTION_COUNT * 2 * Math.PI;
-
-        double farthestDistance = 0;
-        Point farthestPoint = new Point(0, 0);
-        double tickDistance = 0;
-
-        // Reset the two sensors:
-        drive.imu.resetYaw();
-        optical.getMotion();
-
-        double lastYaw = drive.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        while (opModeIsActive() && (rotationTotal < rotationTarget)) {
-            // Track the amount of rotation we've done:
-            double yaw = drive.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-            rotationTotal += Globals.normalizeAngle(yaw - lastYaw);
-            lastYaw = yaw;
-
-            OpticalTrackingPaa5100.Motion motion = optical.getMotion();
-            tickDistance += Math.hypot(motion.x, motion.y);
-
-            currentPoint = currentPoint.add(new Point(motion.x, motion.y).rotate(calibration.correctionAngle + yaw).scale(calibration.inchesPerTick));
-            points.add(currentPoint);
-
-            double currentDistance = Math.hypot(currentPoint.x, currentPoint.y);
-            if (currentDistance > farthestDistance) {
-                farthestDistance = currentDistance;
-                farthestPoint = currentPoint;
+        while (true) {
+            telemetry.addLine("Now ready for the center-of-rotation test.");
+            telemetry.addLine("The robot will need room to spin.\n");
+            telemetry.addLine("Press A to start, B to skip");
+            telemetry.update();
+            while (opModeIsActive() && !buttonA()) {
+                if (buttonB())
+                    return null;
             }
 
-            double rotationsRemaining = (rotationTarget - rotationTotal) / (2 * Math.PI);
-            telemetry.addLine(String.format("%.2f rotations remaining, %d points sampled", rotationsRemaining, points.size()));
+            telemetry.addLine(String.format("Rotating %d times...", REVOLUTION_COUNT));
             telemetry.update();
+
+            ArrayList<Point> points = new ArrayList<>();
+            rampMotors(drive, true);
+
+            Point currentPoint = new Point(0, 0);
+            double rotationTotal = 0;
+            double rotationTarget = REVOLUTION_COUNT * 2 * Math.PI;
+
+            double farthestDistance = 0;
+            Point farthestPoint = new Point(0, 0);
+            double tickDistance = 0;
+
+            // Reset the two sensors:
+            drive.imu.resetYaw();
+            optical.getMotion();
+
+            double lastYaw = drive.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            while (opModeIsActive() && (rotationTotal < rotationTarget)) {
+                // Track the amount of rotation we've done:
+                double yaw = drive.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+                rotationTotal += Globals.normalizeAngle(yaw - lastYaw);
+                lastYaw = yaw;
+
+                OpticalTrackingPaa5100.Motion motion = optical.getMotion();
+                tickDistance += Math.hypot(motion.x, motion.y);
+
+                currentPoint = currentPoint.add(new Point(motion.x, motion.y).rotate(calibration.correctionAngle + yaw).scale(calibration.inchesPerTick));
+                points.add(currentPoint);
+
+                double currentDistance = Math.hypot(currentPoint.x, currentPoint.y);
+                if (currentDistance > farthestDistance) {
+                    farthestDistance = currentDistance;
+                    farthestPoint = currentPoint;
+                }
+
+                // Update the telemetry:
+                double rotationsRemaining = (rotationTarget - rotationTotal) / (2 * Math.PI);
+                telemetry.addLine(String.format("%.2f rotations remaining, %d points sampled", rotationsRemaining, points.size()));
+                telemetry.update();
+
+                // Draw the circle:
+                TelemetryPacket packet = new TelemetryPacket();
+                Canvas canvas = packet.fieldOverlay();
+                double[] xPoints = new double[points.size()];
+                double[] yPoints = new double[points.size()];
+                for (int i = 0; i < points.size(); i++) {
+                    xPoints[i] = points.get(i).x;
+                    yPoints[i] = points.get(i).y;
+                }
+                canvas.setStroke("#00ff00");
+                canvas.strokePolyline(xPoints, yPoints);
+                FtcDashboard.getInstance().sendTelemetryPacket(packet);
+            }
+
+            rampMotors(drive, false);
+
+            CenterOfRotation result = new CenterOfRotation();
+            result.x = -farthestPoint.x / 2;
+            result.y = -farthestPoint.y / 2;
+            result.farthestPointRadius = farthestDistance / 2;
+            result.traveledRadius = tickDistance * calibration.inchesPerTick / (2 * REVOLUTION_COUNT * Math.PI);
+
+            telemetry.addLine(String.format("Test result: Offset from center of rotation (inches): (%.2f, %.2f), samples: %d",
+                    result.x, result.y, points.size()));
+            telemetry.addLine(String.format("Farthest point radius: %.2f, Traveled radius: %.2f", result.farthestPointRadius, result.traveledRadius));
+            telemetry.addLine(String.format("End point (inches): (%.2f, %.2f)\n", currentPoint.x, currentPoint.y));
+
+            telemetry.addLine("Press A to continue, B to repeat this test");
+            telemetry.update();
+            while (opModeIsActive() && !buttonB()) {
+                if (buttonA())
+                    return result; // ====>
+            }
         }
-
-        rampMotors(drive, false);
-
-        double traveledRadius = tickDistance * calibration.inchesPerTick / (2 * REVOLUTION_COUNT * Math.PI);
-
-        CenterOfRotation result = new CenterOfRotation();
-        result.x = -farthestPoint.x / 2;
-        result.y = -farthestPoint.y / 2;
-        result.radius = farthestDistance / 2;
-
-        telemetry.addLine(String.format("Test result: Offset from center of rotation (inches): (%.2f, %.2f), samples: %d",
-                result.x, result.y, points.size()));
-        telemetry.addLine(String.format("Farthest point radius: %.2f, Traveled radius: %.2f", result.radius, traveledRadius));
-        telemetry.addLine(String.format("End point (inches): (%.2f, %.2f)\n", currentPoint.x, currentPoint.y));
-        return result;
     }
 
     @Override
@@ -227,19 +206,13 @@ public class OpticalTrackingTuner extends LinearOpMode {
 
         Calibration calibration = measureCalibration(optical, drive);
         CenterOfRotation centerOfRotation = measureCenterOfRotation(optical, drive, calibration);
-        double radialDistance = measureRadialDistance(optical, drive, calibration);
 
         telemetry.addLine("<< Completed all tuning tests! >>\n");
         telemetry.addLine(String.format("Inches per tick: %f", calibration.inchesPerTick));
         telemetry.addLine(String.format("Correction angle (degrees): %.2f", Math.toDegrees(calibration.correctionAngle)));
         if (centerOfRotation != null) {
             telemetry.addLine(String.format("Offset to center-of-rotation (inches): (%.2f, %.2f)", centerOfRotation.x, centerOfRotation.y));
-        }
-        if (radialDistance >= 0) {
-            telemetry.addLine(String.format("Radial distance (inches): %.2f", radialDistance));
-        }
-        if ((centerOfRotation != null) && (radialDistance >= 0)) {
-            telemetry.addLine(String.format("Center-of-rotation to radial distance ratio: %.2f", radialDistance / centerOfRotation.radius));
+            telemetry.addLine(String.format("\nFarthest-point to distance-traveled radius: %.2f", centerOfRotation.farthestPointRadius / centerOfRotation.traveledRadius));
         }
         telemetry.update();
         while (opModeIsActive())
